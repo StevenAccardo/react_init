88697c07834584ea275e7f85f51a85bb64b2fc2c
Steven Accardo <accardo.steven@gmail.com>
2018-05-30 23:17:56 -0700

    Add more loaders to webpack, and made minor adjustments to file structure

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: '[name].[chunkhash].js'
+    filename: 'src/js/[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -27,24 +27,51 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       },
       {
+        test: /\.(woff(2)?|ttf|eot|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'assets/fonts/[name].[hash].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.pdf$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'assets/docs/[name].[hash].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(mp3|wav|wma|ogg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'assets/audio/[name].[hash].[ext]'
+          }
+        }
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 40000,
-              name: '[name].[hash].[ext]'
+              name: 'assets/images/[name].[hash].[ext]'
             }
           },
           'image-webpack-loader'
         ]
       },
       {
-        test: /\.mp4$/,
+        test: /\.(mp4|webm)$/,
         use: {
           loader: 'file-loader',
           options: {
-            name: '[name].[hash].[ext]'
+            name: 'assets/videos/[name].[hash].[ext]'
           }
         }
       }
